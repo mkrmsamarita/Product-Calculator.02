37f782180411d6f628d33abe674d729f341c843e
mkrmsamarita <mkrmsamarita@tip.edu.ph>
2025-05-18 04:25:37 +0800

    Modified the app.py

--- app.py
+++ app.py
@@ -271,4 +271,6 @@ def remove_ingredients(name):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
